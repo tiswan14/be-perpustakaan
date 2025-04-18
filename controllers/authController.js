3676ad8f88d12fb0bdfc3c58930f6550b7e2560b
tiswan14 <tiswan30@gmail.com>
2025-04-19 05:57:20 +0700

    feat: add getUser function to retrieve user by ID

--- controllers/authController.js
+++ controllers/authController.js
@@ -152,6 +152,34 @@ export const logoutUser = (req, res) => {
     }
 }
 
-export const getUser = (req, res) => {
-    res.send('Get user')
+export const getUser = async (req, res) => {
+    try {
+        const { userId } = req.body
+
+        if (!userId) {
+            return res.status(400).json({
+                success: false,
+                message: 'User ID harus disertakan',
+            })
+        }
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'User tidak ditemukan',
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            data: user,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+    }
 }
