475d5ac171ba1618c7316c67a0d8300bc9e92bda
tiswan14 <tiswan30@gmail.com>
2025-04-18 08:22:53 +0700

    feat: create user model with role-based structure

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    nim: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+
+    role: {
+        type: String,
+        enum: ['admin', 'petugas', 'anggota'],
+        default: 'anggota',
+    },
+})
+
+const userModel = mongoose.models.user || mongoose.model('user', userSchema)
+
+export default userModel
