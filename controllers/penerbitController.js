a2e1e6a0ca4088cf2ad59adca3b8fc849c38d0c1
tiswan14 <tiswan30@gmail.com>
2025-04-22 21:49:12 +0700

    feat(controller): add createPenerbit function

--- controllers/penerbitController.js
+++ controllers/penerbitController.js
@@ -1,5 +1,41 @@
+import Penerbit from '../models/penerbitModel.js'
+
 export const createPenerbit = async (req, res) => {
-    res.send('create penerbit')
+    try {
+        const { nama_penerbit, tahun_terbit } = req.body
+
+        if (!nama_penerbit || !tahun_terbit) {
+            return res.status(400).json({
+                success: false,
+                message: 'Nama penerbit dan tahun terbit harus diisi!',
+            })
+        }
+
+        if (isNaN(tahun_terbit) || tahun_terbit > new Date().getFullYear()) {
+            return res.status(400).json({
+                success: false,
+                message: 'Tahun terbit tidak valid!',
+            })
+        }
+
+        const createdPenerbit = await Penerbit.create({
+            nama_penerbit,
+            tahun_terbit,
+        })
+
+        return res.status(201).json({
+            success: true,
+            message: 'Penerbit berhasil ditambahkan!',
+            data: createdPenerbit,
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            success: false,
+            message:
+                'Terjadi kesalahan saat menambahkan penerbit: ' + error.message,
+        })
+    }
 }
 
 export const getAllPenerbit = async (req, res) => {
